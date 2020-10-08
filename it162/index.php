2784fc76a24b95411f7832152dd76f233a1308e0
Hannah <67710681+hannnahe@users.noreply.github.com>
2020-10-08 11:01:06 -0700

    Files added from portal site first config.

---
+++ it162/index.php
@@ -0,0 +1,21 @@
+
+<?php include "includes/header.php";?>
+
+ <img src="images/desktop.jpg" class="desktop" alt="Hannah on a train" />
+ <img src="images/phone.jpg" class="phone" alt="Hannah at home" />
+    <p>I am a student in the Web Design track at Seattle College. This is only my second class in Web Authoring, but I feel like we learned so much in the first class. I'm excited to keep it up! I started this track this past summer because I am looking to pivot into web design. <br/>
+    I'm most interested in the design aspects of web authoring, probably because I am a textile weaver and love playing with color and shapes and pattern. I'm hoping to gain enough technical skills in web authoring so that I can implement my visual interests. I'm looking forward to learning everything I can in this course.</p>
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <h3>Hobbies</h3>
+ <img src="images/tablet.jpg" class="tablet" alt="Hannah on Mt.Rainier" />
+ <p>Hannah loves hiking and exploring nature in her spare time!
+    She also loves to cook and learn about wine.</p>
+</aside>
+<!-- END RIGHT COL -->
+ 
+<!-- START Footer -->
+<?php include "includes/footer.php";?>
